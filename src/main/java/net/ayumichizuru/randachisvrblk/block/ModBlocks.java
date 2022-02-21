2799930e61e55b76ee234f5fa440327503a663fe
ayumi-chizuru189 <51109272+ayumi-chizuru189@users.noreply.github.com>
2022-02-21 14:30:23 +0700

    added the rest of bed colors, bump version
    (incomplete)

--- src/main/java/net/ayumichizuru/randachisvrblk/block/ModBlocks.java
+++ src/main/java/net/ayumichizuru/randachisvrblk/block/ModBlocks.java
@@ -1,7 +1,6 @@
 package net.ayumichizuru.randachisvrblk.block;
 
 import net.ayumichizuru.randachisvrblk.RandAchiSvrBlk;
-import net.ayumichizuru.randachisvrblk.block.beds.UnrestrictedBaseBed;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.sound.BlockSoundGroup;
@@ -15,10 +14,52 @@ public class ModBlocks {
             new UnrestrictedPistonHead(FabricBlockSettings.of(Material.PISTON).strength(1.5F)));
 
     public static final Block UNRESTRICTED_WHITE_BED = registerBlock("unrestricted_white_bed",
-            new UnrestrictedBaseBed(DyeColor.WHITE , FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+            new UnrestrictedBaseBed(DyeColor.WHITE, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
 
-    // public static final Block UNRESTRICTED_RED_BED = registerBlock("unrestricted_red_bed",
-    //         new UnrestrictedRedBed(DyeColor.RED , FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+     public static final Block UNRESTRICTED_RED_BED = registerBlock("unrestricted_red_bed",
+             new UnrestrictedBaseBed(DyeColor.RED, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_LIGHT_GRAY_BED = registerBlock("unrestricted_light_gray_bed",
+            new UnrestrictedBaseBed(DyeColor.LIGHT_GRAY, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_MAGENTA_BED = registerBlock("unrestricted_magenta_bed",
+            new UnrestrictedBaseBed(DyeColor.MAGENTA, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_YELLOW_BED = registerBlock("unrestricted_yellow_bed",
+            new UnrestrictedBaseBed(DyeColor.YELLOW, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_LIGHT_BLUE_BED = registerBlock("unrestricted_light_blue_bed",
+            new UnrestrictedBaseBed(DyeColor.LIGHT_BLUE, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_PURPLE_BED = registerBlock("unrestricted_purple_bed",
+            new UnrestrictedBaseBed(DyeColor.PURPLE, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_ORANGE_BED = registerBlock("unrestricted_orange_bed",
+            new UnrestrictedBaseBed(DyeColor.ORANGE, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_GREEN_BED = registerBlock("unrestricted_green_bed",
+            new UnrestrictedBaseBed(DyeColor.GREEN, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_BROWN_BED = registerBlock("unrestricted_brown_bed",
+            new UnrestrictedBaseBed(DyeColor.BROWN, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_BLACK_BED = registerBlock("unrestricted_black_bed",
+            new UnrestrictedBaseBed(DyeColor.BLACK, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_PINK_BED = registerBlock("unrestricted_pink_bed",
+            new UnrestrictedBaseBed(DyeColor.PINK, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_LIME_BED = registerBlock("unrestricted_lime_bed",
+            new UnrestrictedBaseBed(DyeColor.LIME, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_GRAY_BED = registerBlock("unrestricted_gray_bed",
+            new UnrestrictedBaseBed(DyeColor.GRAY, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_CYAN_BED = registerBlock("unrestricted_cyan_bed",
+            new UnrestrictedBaseBed(DyeColor.CYAN, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+
+    public static final Block UNRESTRICTED_BLUE_BED = registerBlock("unrestricted_blue_bed",
+            new UnrestrictedBaseBed(DyeColor.BLUE, FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
 
     private static Block registerBlock(String name, Block block) {
         return Registry.register(Registry.BLOCK, new Identifier(RandAchiSvrBlk.MOD_ID, name), block);
---
+++ src/main/java/net/ayumichizuru/randachisvrblk/block/UnrestrictedBaseBed.java
@@ -0,0 +1,135 @@
+package net.ayumichizuru.randachisvrblk.block;
+
+import eu.pb4.polymer.api.block.PolymerBlock;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.BedPart;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class UnrestrictedBaseBed extends BedBlock implements PolymerBlock {
+    public UnrestrictedBaseBed(DyeColor color, Settings settings) {
+        super(color, settings);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return state;
+    }
+
+    @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        this.spawnBreakParticles(world, player, pos, state);
+        if (state.isIn(BlockTags.GUARDED_BY_PIGLINS)) {
+            PiglinBrain.onGuardedBlockInteracted(player, false);
+        }
+
+        world.emitGameEvent(player, GameEvent.BLOCK_DESTROY, pos);
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient) {
+            return ActionResult.CONSUME;
+        } else {
+            if (state.get(PART) != BedPart.HEAD) {
+                pos = pos.offset((Direction)state.get(FACING));
+                state = world.getBlockState(pos);
+                if (!state.isOf(this)) {
+                    return ActionResult.CONSUME;
+                }
+            }
+
+            if ((Boolean)state.get(OCCUPIED)) {
+                if (!this.wakeVillager(world, pos)) {
+                    player.sendMessage(new TranslatableText("block.minecraft.bed.occupied"), true);
+                }
+
+                return ActionResult.SUCCESS;
+            } else {
+                player.trySleep(pos).ifLeft((reason) -> {
+                    if (reason != null) {
+                        player.sendMessage(reason.getMessage(), true);
+                    }
+
+                });
+                return ActionResult.SUCCESS;
+            }
+        }
+    }
+
+    private boolean wakeVillager(World world, BlockPos pos) {
+        List<VillagerEntity> list = world.getEntitiesByClass(VillagerEntity.class, new Box(pos), LivingEntity::isSleeping);
+        if (list.isEmpty()) {
+            return false;
+        } else {
+            ((VillagerEntity)list.get(0)).wakeUp();
+            return true;
+        }
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        Direction direction = ctx.getSide();
+        BlockPos blockPos = ctx.getBlockPos();
+        return (BlockState)((BlockState) this.getDefaultState().with(FACING, ctx.getPlayerFacing())).with(PART, direction != Direction.DOWN && (direction == Direction.UP || !(ctx.getHitPos().y - (double)blockPos.getY() > 0.5D)) ? BedPart.FOOT : BedPart.HEAD);
+    }
+
+    @Override
+    public Block getPolymerBlock(BlockState state) {
+        return getMimicBlocks();
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState state) {
+        return getMimicBlocks().getDefaultState()
+                .with(BedBlock.FACING, state.get(BedBlock.FACING))
+                .with(BedBlock.PART, state.get(BedBlock.PART))
+                .with(BedBlock.OCCUPIED, state.get(BedBlock.OCCUPIED));
+    }
+
+    private Block getMimicBlocks(){
+        return switch (getColor()) {
+            case RED -> Blocks.RED_BED;
+            case LIGHT_GRAY -> Blocks.LIGHT_GRAY_BED;
+            case MAGENTA -> Blocks.MAGENTA_BED;
+            case YELLOW -> Blocks.YELLOW_BED;
+            case LIGHT_BLUE -> Blocks.LIGHT_BLUE_BED;
+            case PURPLE -> Blocks.PURPLE_BED;
+            case ORANGE -> Blocks.ORANGE_BED;
+            case GREEN -> Blocks.GREEN_BED;
+            case BROWN -> Blocks.BROWN_BED;
+            case BLACK -> Blocks.BLACK_BED;
+            case PINK -> Blocks.PINK_BED;
+            case LIME -> Blocks.LIME_BED;
+            case GRAY -> Blocks.GRAY_BED;
+            case CYAN -> Blocks.CYAN_BED;
+            case BLUE -> Blocks.BLUE_BED;
+            default -> Blocks.WHITE_BED;
+        };
+
+    }
+
+}
